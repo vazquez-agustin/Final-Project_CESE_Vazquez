e959e93bae20615b0827cac32d1e35a7ecd5f022
avazquez99 <vazqueza193@gmail.com>
2024-11-24 21:49:33 +0000

    Trying to blink microcontroller's LED. Issue with define

---
+++ WeatherStation_FP/include/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "esp_rom_sys.h" // Library for primitive function to generate delays
+#include "esp_task_wdt.h" // I know this is wrong, do not kill me
+
+#define LED_PIN ----------
--- WeatherStation_FP/src/main.c
+++ WeatherStation_FP/src/main.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
+
+#include "main.h"
+#include "driver/gpio.h"
+
+#define HIGH 1
+#define LOW  0
 
 void app_main() {
 
-    printf("Hello, World!");
+    printf("Hello, World!\n");
 
+    // LED is configured as an output
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while(1)
+    {
+        gpio_set_level(LED_PIN, HIGH);   // Turn LED on
+        printf("LED On.\n");
+        esp_rom_delay_us(500000);     // 500,000 us = 500 ms
+
+        gpio_set_level(LED_PIN, LOW);   // Turn LED off
+        printf("LED Off.\n");
+        esp_rom_delay_us(500000);
+    }
 }
