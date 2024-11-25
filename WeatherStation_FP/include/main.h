cf60910fda9594b036ec627b4251054835f861c4
avazquez99 <vazqueza193@gmail.com>
2024-11-25 19:02:39 +0000

    Small modifications to blinking led code. It works in the terminal, not in the HW

--- WeatherStation_FP/include/main.h
+++ WeatherStation_FP/include/main.h
@@ -2,4 +2,4 @@
 #include "esp_rom_sys.h" // Library for primitive function to generate delays
 #include "esp_task_wdt.h" // I know this is wrong, do not kill me
 
-#define LED_PIN ----------
+#define LED_PIN GPIO_NUM_8
--- WeatherStation_FP/src/main.c
+++ WeatherStation_FP/src/main.c
@@ -1,5 +1,7 @@
 
 #include "main.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "driver/gpio.h"
 
 #define HIGH 1
@@ -10,17 +12,17 @@ void app_main() {
     printf("Hello, World!\n");
 
     // LED is configured as an output
-    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
     while(1)
     {
         gpio_set_level(LED_PIN, HIGH);   // Turn LED on
         printf("LED On.\n");
-        esp_rom_delay_us(500000);     // 500,000 us = 500 ms
+        vTaskDelay(pdMS_TO_TICKS(500));     // 500,000 us = 500 ms
 
         gpio_set_level(LED_PIN, LOW);   // Turn LED off
         printf("LED Off.\n");
-        esp_rom_delay_us(500000);
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
