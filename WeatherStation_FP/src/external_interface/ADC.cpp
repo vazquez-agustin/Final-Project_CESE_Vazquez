429a52a9f066c83db2dd0456b6cca4f227395c6b
Max B <mxbj@pm.me>
2025-02-03 00:12:33 -0300

    fix: removes add_subdirectories and fixes code in .cpp/.h

--- WeatherStation_FP/src/external_interface/ADC.cpp
+++ WeatherStation_FP/src/external_interface/ADC.cpp
@@ -1,40 +1,23 @@
 #include "ADC.h"
 
-class ADC {
-private:
-    adc1_channel_t channel;
-    esp_adc_cal_characteristics_t *adc_chars;
-    adc_unit_t unit;
-    adc_atten_t attenuation;
 
-public:
-    // Variables
-    const int maxADCValue = 4095; // Max ADV value of soil moisture
-    const int refVoltage = 1100;  // With adc2_vref_to_gpio() I could obtain a better estimate
-    // Constructor
-    ADC(adc1_channel_t _channel, adc_unit_t _unit = ADC_UNIT_1, adc_atten_t _attenuation = ADC_ATTEN_DB_11)
-        : channel(_channel), unit(_unit), attenuation(_attenuation) {
-        // ADC initialization
-        adc_chars = (esp_adc_cal_characteristics_t *)calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    }
+ADC::ADC(adc1_channel_t _channel, adc_unit_t _unit, adc_atten_t _attenuation)
+    : channel(_channel), unit(_unit), attenuation(_attenuation) {
+    adc_chars = (esp_adc_cal_characteristics_t *)calloc(1, sizeof(esp_adc_cal_characteristics_t));
+}
 
-    // ADC Configuration
-    void adcSetup() {
-        // ADC width config
-        adc1_config_width(ADC_WIDTH_BIT_12);
-        adc1_config_channel_atten(channel, attenuation);
-        // Characterize ADC
-        esp_adc_cal_characterize(unit, attenuation, ADC_WIDTH_BIT_12, refVoltage, adc_chars);
+ADC::~ADC() {
+    if (adc_chars != nullptr) {
+        free(adc_chars);
     }
+}
 
-    // Read raw value
-    uint32_t readRaw() {
-        return adc1_get_raw((adc1_channel_t)channel);
-    }
+void ADC::adcSetup() {
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(channel, attenuation);
+    esp_adc_cal_characterize(unit, attenuation, ADC_WIDTH_BIT_12, refVoltage, adc_chars);
+}
 
-    ~ADC() {
-        if (adc_chars != nullptr) {
-            free(adc_chars);
-        }
-    }
-};
+uint32_t ADC::readRaw() {
+    return adc1_get_raw((adc1_channel_t)channel);
+}
--- WeatherStation_FP/src/external_interface/ADC.h
+++ WeatherStation_FP/src/external_interface/ADC.h
@@ -1,3 +1,6 @@
+#ifndef ADC_H
+#define ADC_H
+
 #include <stdio.h>
 #include <string.h>
 #include "esp_system.h"
@@ -7,3 +10,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_adc_cal.h"
+
+class ADC {
+private:
+    adc1_channel_t channel;
+    esp_adc_cal_characteristics_t *adc_chars;
+    adc_unit_t unit;
+    adc_atten_t attenuation;
+
+public:
+    const int maxADCValue = 4095;
+    const int refVoltage = 1100;
+
+    ADC(adc1_channel_t _channel, adc_unit_t _unit = ADC_UNIT_1, adc_atten_t _attenuation = ADC_ATTEN_DB_11);
+    ~ADC();
+    
+    void adcSetup();
+    uint32_t readRaw();
+};
+
+#endif // ADC_H
