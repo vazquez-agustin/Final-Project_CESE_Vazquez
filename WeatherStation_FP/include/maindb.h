01a2045efc85c1bf245adc0a63641f7a34c0158b
avazquez99 <vazqueza193@gmail.com>
2025-02-04 00:31:30 +0000

    Temporary name change.

---
+++ WeatherStation_FP/include/maindb.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "esp_system.h"
+#include "driver/adc.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_adc_cal.h"
+#include <time.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Libraries */
+#include <iostream>
+#include <stdio.h>
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "esp_http_client.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MAIN_H
