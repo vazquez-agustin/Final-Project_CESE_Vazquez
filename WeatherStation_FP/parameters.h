8d805d314ec2a575c78e4f3f42416e866462557e
vazquezagostino@gmail.com <vazquezagostino@gmail.com>
2025-03-25 15:45:53 +0000

    Formatting files with license

--- WeatherStation_FP/parameters.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARAMETERS_H
-#define PARAMETERS_H
-
-#define INFLUXDB_URL "http://192.168.178.190:8086/api/v2/write?org=stationtest&bucket=data&precision=s"
-#define INFLUXDB_TOKEN "Lz8RmLxUxfapj08iXf88O8zRWapp21XMNIo7g5HKsQV0JL90MWlzuOOp73VouHUFrDTn-4L74hZWbzcDp69KkQ=="
-#define WIFI_SSID "FRITZ!Box 7530 KW"
-#define WIFI_PASS "43985539165212248477"
-
-#endif // PARAMETERS_H
--- WeatherStation_FP/src/external_interface/ADC.cpp
+++ WeatherStation_FP/src/external_interface/ADC.cpp
@@ -1,4 +1,45 @@
+/************************************************************************************************
+Copyright (c) 2025, Agustín Jesús Vazquez <vazqueza193@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/** @file ADC.cpp
+ ** @brief Implementación de la clase ADC
+ **/
+
+/* --- Headers files inclusions ---------------------------------------------------------------- */
 #include "ADC.h"
+/* --- Macros definitions ---------------------------------------------------------------------- */
+
+/* --- Private data type declarations ---------------------------------------------------------- */
+
+/* --- Private variable declarations ----------------------------------------------------------- */
+
+/* --- Private function declarations ----------------------------------------------------------- */
+
+/* --- Public variable definitions ------------------------------------------------------------- */
+
+/* --- Private variable definitions ------------------------------------------------------------ */
+
+/* --- Private function implementation --------------------------------------------------------- */
+
+/* --- Public function implementation ---------------------------------------------------------- */
 
 ADC::ADC(adc1_channel_t _channel, adc_unit_t _unit, adc_atten_t _attenuation, esp_adc_cal_characteristics_t *_adc_chars)
     : channel(_channel), unit(_unit), attenuation(_attenuation), adc_chars(nullptr) {
