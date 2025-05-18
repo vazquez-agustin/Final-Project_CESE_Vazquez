2cee41f4ef117e6aa821d493068aec267fe5d43b
vazquezagostino@gmail.com <vazquezagostino@gmail.com>
2025-05-18 13:22:12 +0100

    Created defines header

--- WeatherStation_FP/lib/mocks/BME680.cpp
+++ WeatherStation_FP/lib/mocks/BME680.cpp
@@ -33,120 +33,14 @@ SPDX-License-Identifier: MIT
 
 /* === Headers files inclusions ================================================================ */
 #include "BME680.h"
+#include "BME680_defines.h"  
 #include <string.h>
 #include <cstdio>
 // #include "esp_log.h"
 // #include "freertos/FreeRTOS.h"
 // #include "freertos/task.h"
 /* === Macros definitions ====================================================================== */
-/** @brief Puerto I2C. */
-// #define I2C_MASTER_NUM        I2C_NUM_0           
-/** @brief GPIO pin SDA utilizado por I2C. */
-// #define I2C_MASTER_SDA_IO     GPIO_NUM_8
-/** @brief GPIO pin SCL utilizado por I2C. */         
-// #define I2C_MASTER_SCL_IO     GPIO_NUM_9
-/** @brief Frecuencia del reloj I2C. */            
-// #define I2C_MASTER_FREQ_HZ    400000
-/** @brief Dirección I2C del sensor BME680. */       
-#define I2C_REG_ADDRESS       0x77 
-/** @brief Registro para control de humedad en el sensor BME680. */
-#define BME680_REG_CTRL_HUM   0x72
-/** @brief Registro para control de medición en el sensor BME680. */
-#define BME680_REG_CTRL_MEAS  0x74
-/** @brief Dirección de registro utilizado por el sensor BME680 para activar el modo forzado. */
-#define BME680_MODE_FORCED    0x01
 
-/** @brief Dirección de registro del byte más significativo de T1 en el sensor BME680. */
-#define BME680_REG_T1_MSB     0xE9
-/** @brief Dirección de registro del byte menos significativo de T1 en el sensor BME680. */
-#define BME680_REG_T1_LSB     0xEA
-/** @brief Dirección de registro del byte más significativo de T2 en el sensor BME680. */
-#define BME680_REG_T2_MSB     0x8A
-/** @brief Dirección de registro del byte menos significativo de T2 en el sensor BME680. */
-#define BME680_REG_T2_LSB     0x8B
-/** @brief Dirección de registro del byte T3 en el sensor BME680. */
-#define BME680_REG_T3         0x8C
-/** @brief Dirección de registro del byte menos significativo (XLSB) de temperatura cruda en el sensor BME680. */
-#define BME680_REG_TRAW_XLSB  0x24
-/** @brief Dirección de registro del byte menos significativo (LSB) de temperatura cruda en el sensor BME680. */
-#define BME680_REG_TRAW_LSB   0x23
-/** @brief Dirección de registro del byte más significativo (MSB) de temperatura cruda en el sensor BME680. */
-#define BME680_REG_TRAW_MSB   0x22
-
-/** @brief Dirección de registro del byte más significativo de P1 en el sensor BME680. */
-#define BME680_REG_P1_MSB     0x8F
-/** @brief Dirección de registro del byte menos significativo de P1 en el sensor BME680. */
-#define BME680_REG_P1_LSB     0x8E
-/** @brief Dirección de registro del byte más significativo de P2 en el sensor BME680. */
-#define BME680_REG_P2_MSB     0x91
-/** @brief Dirección de registro del byte menos significativo de P2 en el sensor BME680. */
-#define BME680_REG_P2_LSB     0x90
-/** @brief Dirección de registro del byte P3 en el sensor BME680. */
-#define BME680_REG_P3         0x92
-/** @brief Dirección de registro del byte más significativo de P4 en el sensor BME680. */
-#define BME680_REG_P4_MSB     0x95
-/** @brief Dirección de registro del byte menos significativo de P4 en el sensor BME680. */
-#define BME680_REG_P4_LSB     0x94
-/** @brief Dirección de registro del byte más significativo de P5 en el sensor BME680. */
-#define BME680_REG_P5_MSB     0x97
-/** @brief Dirección de registro del byte menos significativo de P5 en el sensor BME680. */
-#define BME680_REG_P5_LSB     0x96
-/** @brief Dirección de registro del byte P6 en el sensor BME680. */
-#define BME680_REG_P6         0x99
-/** @brief Dirección de registro del byte P7 en el sensor BME680. */
-#define BME680_REG_P7         0x98
-/** @brief Dirección de registro del byte menos significativo de P8 en el sensor BME680. */
-#define BME680_REG_P8_MSB     0x9D
-/** @brief Dirección de registro del byte menos significativo de P8 en el sensor BME680. */
-#define BME680_REG_P8_LSB     0x9C
-/** @brief Dirección de registro del byte menos significativo de P9 en el sensor BME680. */
-#define BME680_REG_P9_MSB     0x9F
-/** @brief Dirección de registro del byte menos significativo de P9 en el sensor BME680. */
-#define BME680_REG_P9_LSB     0x9E
-/** @brief Dirección de registro del byte P10 en el sensor BME680. */
-#define BME680_REG_P10        0xA0
-/** @brief Dirección de registro del byte menos significativo (XLSB) de presión cruda en el sensor BME680. */
-#define BME680_REG_PRAW_XLSB  0x21
-/** @brief Dirección de registro del byte menos significativo (LSB) de presión cruda en el sensor BME680. */
-#define BME680_REG_PRAW_LSB   0x20
-/** @brief Dirección de registro del byte más significativo (MSB) de presión cruda en el sensor BME680. */
-#define BME680_REG_PRAW_MSB   0x1F
-
-/** @brief Dirección de registro del byte más significativo de H1 en el sensor BME680. */
-#define BME680_REG_H1_MSB     0xE3
-/** @brief Dirección de registro del byte menos significativo de H1 en el sensor BME680. */
-#define BME680_REG_H1_LSB     0xE2
-/** @brief Dirección de registro del byte más significativo de H2 en el sensor BME680. */
-#define BME680_REG_H2_MSB     0xE1
-/** @brief Dirección de registro del byte menos significativo de H2 en el sensor BME680. */
-#define BME680_REG_H2_LSB     0xE2
-/** @brief Dirección de registro del byte H3 en el sensor BME680. */
-#define BME680_REG_H3         0xE4
-/** @brief Dirección de registro del byte H4 en el sensor BME680. */
-#define BME680_REG_H4         0xE5
-/** @brief Dirección de registro del byte H5 en el sensor BME680. */
-#define BME680_REG_H5         0xE6
-/** @brief Dirección de registro del byte H6 en el sensor BME680. */
-#define BME680_REG_H6         0xE7
-/** @brief Dirección de registro del byte H7 en el sensor BME680. */
-#define BME680_REG_H7         0xE8
-/** @brief Dirección de registro del byte menos significativo (LSB) de humedad cruda en el sensor BME680. */
-#define BME680_REG_HRAW_MSB   0x25
-/** @brief Dirección de registro del byte más significativo (MSB) de humedad cruda en el sensor BME680. */
-#define BME680_REG_HRAW_LSB   0x26
-
-/** @brief Dirección de registro del byte más significativo (MSB) de humedad cruda en el sensor BME680. */
-#define BME680_OSR_SKIPPED    0x00
-/** @brief Dirección de registro del oversampling de 1x para el sensor BME680. */
-#define BME680_OSR_1X         0x01
-/** @brief Dirección de registro del oversampling de 2x para el sensor BME680. */
-#define BME680_OSR_2X         0x02
-/** @brief Dirección de registro del oversampling de 4x para el sensor BME680. */
-#define BME680_OSR_4X         0x03
-/** @brief Dirección de registro del oversampling de 8x para el sensor BME680. */
-#define BME680_OSR_8X         0x04
-/** @brief Dirección de registro del oversampling de 16x para el sensor BME680. */
-#define BME680_OSR_16X        0x05
 /* === Private data type declarations ========================================================== */
 
 /* === Private variable declarations =========================================================== */
@@ -492,3 +386,5 @@ bool BME680::getMeasure(measure_t *data) {
 
     return true;
 }
+
+/* === End of documentation ==================================================================== */
