0d9a8702362f666d3481f43426008ea9a2b04798
vazquezagostino@gmail.com <vazquezagostino@gmail.com>
2025-05-20 12:31:23 +0100

    Data logger mock to test

---
+++ WeatherStation_FP/lib/mocks/DataLoggerMock.cpp
@@ -0,0 +1,10 @@
+#include "DataLogger_interface.h"
+
+class DataLogger: public DataLogger_interface {
+    private:
+
+    public:
+        void logEspLog(char* msg1, char* msg2) {
+            return;
+        } 
+};
