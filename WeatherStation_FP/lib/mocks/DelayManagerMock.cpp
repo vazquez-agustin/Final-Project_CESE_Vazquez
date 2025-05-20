7bf1c5ec398779ceb74f90fbe9387d85122cea1e
vazquezagostino@gmail.com <vazquezagostino@gmail.com>
2025-05-20 18:21:04 +0100

    Delay manager mock to test

---
+++ WeatherStation_FP/lib/mocks/DelayManagerMock.cpp
@@ -0,0 +1,10 @@
+#include "DelayManager_interface.h"
+
+class DelayManager: public DelayManager_interface {
+    private:
+
+    public:
+        void delay(uint32_t ms) {
+            return;
+        }
+};
