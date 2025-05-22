b0d1a172e40a412917e6634d02e30f99116528e4
vazquezagostino@gmail.com <vazquezagostino@gmail.com>
2025-05-22 22:56:17 +0100

    Files that control external lib functions moved to external_lib folder

--- WeatherStation_FP/lib/mocks/DataLogger.cpp
+++
@@ -1,6 +0,0 @@
-#include "esp_log.h"
-#include "DataLogger.h"
-
-void DataLogger::logEspLog(char* msg1, char* msg2) {
-            ESP_LOGE(msg1, "%s", msg2);
-        }
--- WeatherStation_FP/lib/mocks/DelayManager.cpp
+++
@@ -1,7 +0,0 @@
-#include "DelayManager.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-void DelayManager::delay(uint32_t ms) {
-    vTaskDelay(pdMS_TO_TICKS(ms));
-}
