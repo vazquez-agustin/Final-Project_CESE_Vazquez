ba4befb4759479cadff67f16048624c79f12b700
vazquezagostino@gmail.com <vazquezagostino@gmail.com>
2025-07-03 19:15:01 +0100

    Fixed send_data

--- WeatherStation_FP/src/database/influxdb.cpp
+++ WeatherStation_FP/src/database/influxdb.cpp
@@ -58,7 +58,7 @@ void InfluxDBClient::send_data(uint32_t moisture_value, uint32_t speed_value, ui
     // String with the data to send to InfluxDB
     std::string post_data = "humedad_de_suelo,location=outside value=" + std::to_string(moisture_value) + "\n" +
                             "velocidad_de_viento,location=outside value=" + std::to_string(speed_value) + "\n" +
-                            "direccion_de_viento,location=outside value=" + std::to_string(wind_direction_value) +
+                            "direccion_de_viento,location=outside value=" + std::to_string(wind_direction_value) + "\n" +
                             "temperatura,location=outside value=" + std::to_string(temperature_value) + "\n" +
                             "humedad,location=outside value=" + std::to_string(humidity_value) + "\n" +
                             "presion,location=outside value=" + std::to_string(pressure_value);
